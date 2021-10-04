e4c0ddc61a7a8d6d40b772b5fc94a9f5c2ad3209
Marco Kellershoff <marco@kellershoff.net>
2021-10-04 13:47:40 +0200

    Fix navigator.msMaxTouchPoints is not defined

--- custom.d.ts
+++ custom.d.ts
@@ -1,4 +1,8 @@
-declare module "*.svg" {
-  const content: any;
-  export default content;
+declare module '*.svg' {
+  const content: any
+  export default content
+}
+
+interface Navigator extends Navigator {
+  msMaxTouchPoints: number
 }
